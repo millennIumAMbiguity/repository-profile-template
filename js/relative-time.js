03cdd3d59f467807153271d5c27609c3e312caa1
millennIumAMbiguity <37588844+millennIumAMbiguity@users.noreply.github.com>
2025-02-19 00:46:11 +0100

    Update relative-time.js

--- js/relative-time.js
+++ js/relative-time.js
@@ -1,21 +1,21 @@
 export function timeDifference(current, previous) {
-  var msPerMinute = 60 * 1000;
-  var msPerHour = msPerMinute * 60;
-  var msPerDay = msPerHour * 24;
-  var msPerMonth = msPerDay * 30;
-  var msPerYear = msPerDay * 365;
+  const msPerMinute = 60 * 1000;
+  const msPerHour = msPerMinute * 60;
+  const msPerDay = msPerHour * 24;
+  const msPerMonth = msPerDay * 30;
+  const msPerYear = msPerDay * 365;
 
-  var elapsed = current - previous;
+  const elapsed = current - previous;
 
-  if (elapsed < msPerMinute) {
+  if (elapsed <= msPerMinute) {
     return Math.round(elapsed / 1000) + " seconds ago";
-  } else if (elapsed < msPerHour) {
+  } else if (elapsed <= msPerHour) {
     return Math.round(elapsed / msPerMinute) + " minutes ago";
-  } else if (elapsed < msPerDay) {
+  } else if (elapsed <= msPerDay) {
     return Math.round(elapsed / msPerHour) + " hours ago";
-  } else if (elapsed < msPerMonth) {
+  } else if (elapsed <= msPerMonth) {
     return Math.round(elapsed / msPerDay) + " days ago";
-  } else if (elapsed < msPerYear) {
+  } else if (elapsed <= msPerYear) {
     return Math.round(elapsed / msPerMonth) + " months ago";
   } else {
     return Math.round(elapsed / msPerYear) + " years ago";
