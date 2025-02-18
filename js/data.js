e4af5eeccb03d2c7769a3100ccffcab84c61ae43
millennIumAMbiguity <37588844+millennIumAMbiguity@users.noreply.github.com>
2025-02-18 22:30:22 +0100

    Added site

---
+++ js/data.js
@@ -0,0 +1,22 @@
+export const repoData = {
+  "millennIumAMbiguity/ctrvcr":
+    "https://cdn.modrinth.com/data/XaonJnsc/50f76c6946633c8910a50cac689d4329d37dc9df.gif",
+  "millennIumAMbiguity/HorizontalDoors":
+    "https://github.com/millennIumAMbiguity/HorizontalDoors/blob/1.20.4/common/src/main/resources/horizontaldoors.png?raw=true",
+  "millennIumAMbiguity/image-url-replacer":
+    "https://img.itch.zone/aW1nLzg3MzcyMzgucG5n/105x83%23/rljzcO.png",
+  "millennIumAMbiguity/no-block-tomorrow":
+    "https://img.itch.zone/aW1nLzcyNzQyMDAucG5n/105x83%23/X0Qy%2Fw.png",
+  "millennIumAMbiguity/no-more-knockback-latency":
+    "https://github.com/millennIumAMbiguity/no-more-knockback-latency/blob/1.20.6/common/src/main/resources/nomoresingleplayerlatency.png?raw=true",
+  "millennIumAMbiguity/smart-error-code-generator":
+    "https://github.com/millennIumAMbiguity/smart-error-code-generator/blob/master/assets/icon.png?raw=true",
+  "savatkinv/VoxelGame":
+    "https://github.com/savatkinv/VoxelGame/blob/master/VoxelGame_UnityProject/Assets/VoxelGame/Publish/Icon/icon.png?raw=true",
+  "ValkyrienSkies/Eureka":
+    "https://cdn.modrinth.com/data/EO8aSHxh/67865e0eabadcd6903f2a3296b729ba00b23de3e.png",
+  "millennIumAMbiguity/no-more-packs":
+    "https://cdn.modrinth.com/data/Up4a09Dq/8f0a74e142dd8f94edd544e0d26297341c5d5c37.png",
+};
+
+export const additionalRepos = ["savatkinv/VoxelGame", "ValkyrienSkies/Eureka"];
