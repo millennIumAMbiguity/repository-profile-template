2b7baf109b861da8f71ad0ec94872598eb958bc0
millennIumAMbiguity <37588844+millennIumAMbiguity@users.noreply.github.com>
2025-02-19 01:02:49 +0100

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,33 +1,13 @@
+// replace with your username
 export const username = "millennIumAMbiguity";
 
+// Additional data for repos.
 export const repoData = {
-  "millennIumAMbiguity/ctrvcr": {
-    "icon": "https://cdn.modrinth.com/data/XaonJnsc/50f76c6946633c8910a50cac689d4329d37dc9df.gif",
-  },
-  "millennIumAMbiguity/HorizontalDoors": {
-    "icon": "https://github.com/millennIumAMbiguity/HorizontalDoors/blob/1.20.4/common/src/main/resources/horizontaldoors.png?raw=true",
-  },
-  "millennIumAMbiguity/image-url-replacer": {
-    "icon": "https://img.itch.zone/aW1nLzg3MzcyMzgucG5n/105x83%23/rljzcO.png",
-  },
-  "millennIumAMbiguity/no-block-tomorrow": {
-    "icon": "https://img.itch.zone/aW1nLzcyNzQyMDAucG5n/105x83%23/X0Qy%2Fw.png",
-  },
-  "millennIumAMbiguity/no-more-knockback-latency": {
-    "icon": "https://github.com/millennIumAMbiguity/no-more-knockback-latency/blob/1.20.6/common/src/main/resources/nomoresingleplayerlatency.png?raw=true",
-  },
-  "millennIumAMbiguity/smart-error-code-generator": {
-    "icon": "https://github.com/millennIumAMbiguity/smart-error-code-generator/blob/master/assets/icon.png?raw=true",
-  },
-  "savatkinv/VoxelGame": {
-    "icon": "https://github.com/savatkinv/VoxelGame/blob/master/VoxelGame_UnityProject/Assets/VoxelGame/Publish/Icon/icon.png?raw=true",
-  },
-  "ValkyrienSkies/Eureka": {
-    "icon": "https://cdn.modrinth.com/data/EO8aSHxh/67865e0eabadcd6903f2a3296b729ba00b23de3e.png",
-  },
-  "millennIumAMbiguity/no-more-packs": {
-    "icon": "https://cdn.modrinth.com/data/Up4a09Dq/8f0a74e142dd8f94edd544e0d26297341c5d5c37.png",
-  },
+  // Set a custom icon like this:
+  "millennIumAMbiguity/repository-profile-template": {
+    "icon": "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+  }
 };
 
-export const additionalRepos = ["savatkinv/VoxelGame", "ValkyrienSkies/Eureka"];
+// Adding a repo to this list will make it appear on your profile even though you are not the owner.
+export const additionalRepos = ["millennIumAMbiguity/repository-profile-template"];
